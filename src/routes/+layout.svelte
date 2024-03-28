40b25e22e21963f4d081bbc8f972b78e84490cd0
matthew lee <laughew@gmail.com>
2024-03-28 14:40:11 -0700

    font

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<slot />
+
+<style>
+	@font-face {
+		font-family: 'Work Sans';
+		font-style: normal;
+		src: url('/fonts/WorkSans-Variable.ttf'); /* IE9 Compat Modes */
+		src:
+			local(''),
+			url('/fonts/WorkSans-Variable.ttf?#iefix') format('truetype'),
+	}
+
+	:global(body) {
+		font-family: 'Work Sans', sans-serif;
+	}
+</style>
