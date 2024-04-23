f2394854360cb2a65d96a530d369a884a589b703
matthew lee <laughew@gmail.com>
2024-04-23 10:22:34 -0700

    added redirects + cleaned up error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div id="error">
+	<h1>{$page.status}</h1>
+	<p>{$page.error.message}</p>
+</div>
+
+<style>
+	#error {
+		padding-top: 16rem;
+		padding-bottom: 16rem;
+		text-align: center;
+	}
+</style>
