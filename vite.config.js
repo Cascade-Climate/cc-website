7c090baaf2faac7ce657ae3afb651a2e710741c6
matthew lee <laughew@gmail.com>
2024-09-30 16:18:38 -0400

    config stuff

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { loadEnv } from 'vite';
 
-export default defineConfig(({ mode }) => {
-	process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-	return {
-		plugins: [sveltekit()]
-	};
+export default defineConfig({
+	plugins: [sveltekit()]
 });
