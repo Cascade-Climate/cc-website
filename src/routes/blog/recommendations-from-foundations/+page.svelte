280170732f315423cecf2e712bf4b5a91c159e63
arielle <73043247+ariellelok@users.noreply.github.com>
2025-06-01 23:34:10 -0400

    new page

---
+++ src/routes/blog/recommendations-from-foundations/+page.svelte
@@ -0,0 +1,177 @@
+<script>
+    import figure1 from '$lib/images/blog/figure-1.webp';
+    import figure2 from '$lib/images/blog/figure-2.webp';
+	import PDFLink from '$lib/components/PDFLink.svelte';
+	import ScrollNav from '$lib/components/ScrollNav.svelte';
+	import image1 from '$lib/images/blog/increasing-trust-decreasing-cost.webp';
+</script>
+
+<svelte:head>
+    <title>Foundations for a Healthy ERW Market Cycle</title> 
+</svelte:head>
+
+<div>
+	<section class="intro">
+		<div class="content">
+			<div class="copy">
+				<h1>Recommendations from <em>Foundations</em> as the EU Explores Enhanced Rock Weathering</h1>
+				<p class="date">June 2, 2025</p>
+				<hr>
+			</div>
+		</div>
+	</section>
+	<main>
+	<p></p>
+    <p>At the last EU Commission Expert Group on permanent carbon removals, the EU Commission signaled the start of a technical scoping process for an Enhanced Rock Weathering (ERW) methodology under the Carbon Removals and Carbon Farming (CRCF) regulation. This is a welcome step forward. The EU CRCF regulation will create a standardized and transparent EU-wide certification framework for carbon removals, representing a significant opportunity to raise the profile of ERW as a promising permanent carbon removal pathway and incorporate the most recent scientific developments into the first EU-wide ERW methodology.</p>
+    <br>
+    <p>Cascade&rsquo;s <a href="https://cascadeclimate.org/blog/foundations-for-carbon-removal-quantification-in-erw-deployments" target="_blank" rel="noopener"><em>Foundations for Carbon Dioxide Removal Quantification in ERW Deployments</em></a> provides a valuable starting point for developing an EU CRCF ERW methodology. This comprehensive framework represents the culmination of a collaborative, multi-stakeholder process involving approximately 50 academic scientists, 20 ERW project developers, and various not-for-profit organizations. <em>Foundations </em>represents the most current community-wide consensus on best practices for measurement, reporting, and verification (MRV) of net carbon dioxide removal in ERW projects.</p>
+    <br>
+    <p>We believe that <a href="https://cascadeclimate.org/blog/rigorous-mrv-methodologies" target="_blank" rel="noopener">rigorous MRV methodologies can and must be developed</a> for ERW and that a high quality methodology under the EU CRCF will send a clear signal to the market about minimum requirements for robust quantification and environmental health and safety.&nbsp;</p>
+    <h2>What is enhanced rock weathering?</h2>
+    <p>ERW involves spreading finely crushed silicate or carbonate rock&mdash;such as basalt, olivine, and limestone&ndash;onto fields, which speeds up the natural process of breaking down or &ldquo;weathering&rdquo; rocks by increasing their reactive surface area. Under many conditions, this weathering absorbs CO2 from the atmosphere, durably storing the carbon as bicarbonate in the ocean on the timescale of tens of thousands of years. ERW also has the potential to deliver agronomic benefits to farmers, such as <a href="https://www.mdpi.com/2073-445X/13/11/1839" target="_blank" rel="noopener">pH control</a>, <a href="https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0295031" target="_blank" rel="noopener">improved nutrient availability</a>, and&mdash;through these benefits&mdash;increased crop yield.</p>
+
+
+    <h5><strong>Figure 1. Overview of ERW carbon removal process.</strong></h5>
+    <div style="display: flex; justify-content: center; width: 100%;">
+        <img src={figure1} alt="Overview of ERW carbon removal process" style="width: 100%;" />
+    </div>
+    <hr><br>
+    <h2>Key Recommendations from Foundations</h2>
+
+    <h3>1. Account for all terms of the net carbon balance&nbsp;</h3>
+
+    <p><em>Foundations</em> provides a clear framework for measuring and calculating net carbon dioxide removal (CDR) from an ERW project. Net CDR is calculated by measuring potential carbon dioxide removal in the upper soil region (referred to as the &ldquo;near-field zone&rdquo;) and subtracting out the following deductions:</p>
+    <ul>
+    <li><strong>Life cycle emissions:</strong> Greenhouse gas emissions associated with the production, transport, handling, and spreading of ERW feedstock materials.</li>
+    <li><strong>Counterfactual CDR:</strong> Carbon removal that would have occurred in the absence of the ERW project.</li>
+    <li><strong>Near-field zone losses: </strong>Carbon losses that occur in the upper soil region.</li>
+    <li><strong>Far-field zone losses: </strong>Carbon losses that occur on the path from the upper soil region to a durable carbon storage reservoir.</li>
+    </ul>
+
+
+    <h5><strong>Figure 2. Overview of terms for calculating net CDR in an ERW deployment.&nbsp;</strong></h5>
+    <div style="display: flex; justify-content: center; width: 100%;">
+        <img src={figure2} alt="Overview of terms for calculating net CDR in an ERW deployment" style="width: 100%;" />
+    </div>
+    <br>
+    <p>Importantly, ERW projects receive carbon removal credits on a net basis&mdash;only <em>after </em>measurement of potential CDR in the upper soil region and the subtraction of all loss factors. Table 1 lists the &ldquo;deductions&rdquo; that should be considered along the path from the upper soil to the durable carbon storage reservoir.</p>
+    <br><p>While soil organic carbon (SOC) is not listed below, Cascade recognizes it as an important monitoring and research priority. There are certain conditions that increase the likelihood of SOC loss from an ERW deployment, such as when existing SOC is greater than 5% of total soil mass and in some higher-risk soil types (see <a href="https://cascadeclimate.org/blog/foundations-for-carbon-removal-quantification-in-erw-deployments" target="_blank" rel="noopener"><em>Foundations</em></a>). An EU CRCF ERW methodology should consider restrictions on deployments in these higher-risk contexts. Additionally, to advance scientific understanding of ERW's effects on soil carbon across diverse environments, we encourage requiring SOC measurement for all ERW projects, with results submitted to <a href="https://cascadeclimate.org/blog/data-sharing-system-for-erw" target="_blank" rel="noopener">Data Quarry</a> or another publicly accessible database.</p>
+    <br><p>Many of the deductions in the upper soil account for instances where cations (positively charged ions such as calcium and magnesium) are lost through various processes. As cations are lost in the soil profile, this leads to a reduction in carbon-containing ions, called bicarbonate, which ultimately reduces the net amount of carbon stored. <strong>We recommend an EU CRCF methodology account for all deductions in the near-field and far-field zones listed in Table 1.</strong></p>
+    <br><h5><strong>Table 1. Deductions in the near-field zone and far-field zone when calculating net carbon dioxide removal from an ERW deployment.</strong></h5>
+    <table style="min-width: 75px; border: 1px solid black; border-collapse: collapse;">
+        <colgroup>
+            <col>
+            <col>
+            <col>
+        </colgroup>
+        <tbody>
+            <tr>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p></p>
+                </td>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p><strong>Deduction term</strong></p>
+                </td>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p><strong>Description<sup>1</sup></strong></p>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="1" rowspan="5" style="padding: 8px; border: 1px solid black; vertical-align: middle;">
+                    <p><strong>Near-field zone (<em>upper soil region</em>)</strong></p>
+                </td>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>Weathering</p>
+                    <p>by non-carbonic (i.e., non-CO2) acids and in low pH soils</p>
+                </td>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>Rock weathering that does not result in carbon sequestration (e.g., in very acidic soils or by non-carbonic acids)</p>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>Cation Sorption</p>
+                </td>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>Loss of cations that attach to soil particle surfaces</p>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>Secondary Carbonate Formation<sup>2</sup></p>
+                </td>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>Loss of cations through the formation of solid carbonate minerals (e.g., calcite)</p>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>Secondary Phase Formation</p>
+                </td>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>Loss of cations through the formation of clays and other secondary phases</p>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>Biomass Uptake</p>
+                </td>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>Plant uptake of cations as nutrients</p>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="1" rowspan="3" style="padding: 8px; border: 1px solid black; vertical-align: middle;">
+                    <p><strong>Far Field Zone (<em>lower soil region to the carbon storage reservoir</em>)</strong></p>
+                </td>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>Deeper Soil and Groundwater Loss</p>
+                </td>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>Carbon and cation loss in the lower soil and groundwater</p>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>River Loss</p>
+                </td>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>Carbon loss to the atmosphere caused by pH re-balancing and carbonate mineral precipitation in surface water systems</p>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>Ocean Loss</p>
+                </td>
+                <td colspan="1" rowspan="1" style="padding: 8px; border: 1px solid black;">
+                    <p>Carbon loss due to pH re-balancing in the coastal ocean</p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <br>
+    <ol>
+    <em><li>Some of these deduction terms are implicitly included in different measurement approaches and thus don't need to be explicitly subtracted.</li></em>
+    <em><li>This process can lead to both carbon storage and carbon losses, but tends to be a net carbon loss.</li></em>
+    </ol>
+    <br>
+    <h3>2. Allow measurement flexibility for robust quantification</h3>
+
+    <p>In many ERW deployments, multiple quantification methods&mdash;including measurements of the solid, liquid, and gas phases in the soil&mdash;are used to provide viewpoints into various aspects of the system. Current CDR quantification methodologies in the voluntary carbon market primarily allow for either soil core (solid phase) or porewater sampling (liquid phase) approaches. Additional measurement techniques, such as ion exchange resins and soil CO2 sensors, are currently under development. Regardless of measurement type, sampling density should be determined via a baseline site variability assessment with measurement uncertainty transparently accounted for in carbon credit calculations.</p>
+    <br><p>Each method comes with its own technical and operational strengths and challenges, which are discussed in detail in <em>Foundations, </em>as well as <a href="https://www.frontiersin.org/journals/climate/articles/10.3389/fclim.2024.1345224/full" target="_blank" rel="noopener">Clarkson et al. (2023)</a>. The optimal measurement approach for accurately quantifying CDR depends on site-specific factors including soil type, regional climate, and agronomic management practices. <strong>We recommend that an EU CRCF methodology allow flexibility in measurement techniques to ensure robust CDR quantification across varying deployment conditions.&nbsp;</strong></p>
+    <br>
+    <h3>3. Safeguard against heavy metal accumulation</h3>
+
+    <p>As the crushed rocks from ERW deployments break down over time and carbon is removed from the atmosphere, various metals within these rocks are also released. Accumulated metals can potentially cause environmental and human health risks at high concentrations. <em>Foundations</em> recommends every ERW deployment produce a health and safety risk assessment prior to the start of a project. Since <em>Foundations</em> was published, Cascade released an ERW <a href="https://cascadeclimate.org/blog/metal-accumulation-calculator" target="_blank" rel="noopener">metal accumulation calculator</a>, which uses intentionally conservative assumptions to compare potential accumulation of metals against existing regulations.&nbsp;</p>
+    <br><p>In the EU CRCF context, <strong>we recommend requiring project developers to perform a conservative mass balance calculation using the baseline soil and feedstock measurements and soil conditions prior to application.</strong> This will assist in verifying that the proposed deployment will not cause soil metal concentrations to exceed the regulatory limits<strong> </strong>dictated by the EU <em>Council Directive on the Protection of the Environment, and in Particular of the Soil, when Sewage Sludge is used in Agriculture</em></p>
+    <p>(<a href="https://eur-lex.europa.eu/eli/dir/1986/278/oj/eng" target="_blank" rel="noopener">Council Directive 86/278/EEC</a>) or the regulatory limits of the jurisdiction in which the project takes place.&nbsp;</p>
+    <br><p><span style="color: #1d1c1d;">If baseline sampling indicates soil metal concentrations that are above the regulatory soil thresholds of the relevant jurisdiction, and the ERW deployment will exacerbate metal concentrations in soils, we recommend that the project developer select an alternative deployment site.</span></p>
+    <br>
+    <h3>4. Ensure methodology updates keep pace with scientific progress</h3>
+
+    <p>While strong scientific foundations exist for ERW quantification today, we anticipate continued advancements that will lead to the evolution of MRV best practices over time. For example, there should be flexibility in the measurement requirements to allow for incorporation of new MRV approaches (e.g., the use of ion exchange resins) as they are proven out. <strong>We recommend an EU CRCF ERW methodology maintain enough flexibility to incorporate future scientific developments as part of a recurring review process </strong>(e.g., every two years).</p>
+    <br><h2>What&rsquo;s next?</h2>
+    <p>Building on the framework established in <em>Foundations</em>, the EU has the opportunity to write a high-quality, rigorous methodology for ERW. A well-designed CRCF methodology will establish clear standards for robust measurement and environmental safety, providing market certainty while ensuring ERW projects deliver genuine carbon removal with appropriate environmental safeguards.&nbsp;</p>
+    <br><p>Cascade is committed to supporting this process through ongoing research and collaborative engagement. We are actively engaging the scientific community in exploring several critical areas relevant for advancing ERW methodologies, including quantification approaches for secondary silicate materials, rigorous intercomparison of measurement techniques, and understanding of soil organic carbon dynamics in ERW systems. Furthermore, we look forward to partnering with policymakers as they develop high rigor methodologies for ERW and build ERW into their net zero pathways.&nbsp;&nbsp;</p>
+</main>
+</div>
