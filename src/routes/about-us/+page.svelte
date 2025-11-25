416e2a3c43d3c55311fb701aa68161966350c1b8
jonjonmoore <JONATHAN@CASCADECLIMATE.ORG>
2025-11-25 13:04:09 -0500

    Update +page.svelte

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -83,6 +83,9 @@
 		<a href="https://www.mcgovern.org/">
 			<img width="680" height="365" src={mcgovernLogo} alt="Patrick J McGovern Foundation Logo" />
 		</a>
+		<a href="https://www.navigationfund.org/">
+			<img width="680" height="365" src={navigationLogo} alt="Navigation Fund Logo" />
+		</a>
 		<a href="https://qc.foundation/">
 			<img width="680" height="365" src={quadratureLogo} alt="Quadrature Climate Foundation Logo" />
 		</a>
