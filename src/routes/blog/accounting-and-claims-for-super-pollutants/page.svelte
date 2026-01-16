75936e41af5b359e959d5f0fb13d1b487242406d
jonjonmoore <JONATHAN@CASCADECLIMATE.ORG>
2026-01-16 11:01:18 -0500

    Create page.svelte

---
+++ src/routes/blog/accounting-and-claims-for-super-pollutants/page.svelte
@@ -0,0 +1,97 @@
+<script>
+	import pipes from '$lib/images/blog/pipes.webp';
+	import PDFLink from '$lib/components/PDFLink.svelte';
+	import ScrollNav from '$lib/components/ScrollNav.svelte';
+	import policy from '$lib/images/nature/policy.webp';
+</script>
+
+<svelte:head>
+    <title>Accounting and Claims</title> 
+</svelte:head>
+
+<div>
+	<section class="intro">
+		<div class="content">
+			<div class="copy">
+				<h1>Accounting and Claims for Super Pollutants</h1>
+				<p>
+					As carbon markets shift from voluntary finance to policy-driven accountability, a critical challenge emerges: ensuring consistent, rigorous accounting of both short- and long-lived greenhouse gases.
+				</p>
+				<h4>Latest updates:</h4>
+				<div class="links">
+					<a href="/blog/crosssing-the-bridge">Sep 16, 2025: Crossing the Bridge: How Voluntary Climate Finance Can Accelerate Sustained Policy and Industry Adoption↗</a>
+				</div>
+			</div>
+			<img src={pipes} alt="Pipes"/>
+		</div>
+	</section>
+	<ScrollNav />
+	<main>
+		<section id="policyPriorities">
+			<h1>Policy Priorities for ERW</h1>
+			<div style="display: flex; flex-direction: column; gap: 10px; width: 100%;">
+				<PDFLink link="/Cascade Climate - Policy Priorities for Enhanced Rock Weathering - Executive Summary.pdf" variant="accent" style="display: block; text-align: left;">[Executive Summary] Policy Priorities for Enhanced Rock Weathering↗</PDFLink>
+				<PDFLink link="/Cascade Climate - Policy Priorities for Enhanced Rock Weathering.pdf" variant="accent" style="display: block; text-align: left;">[Full] Policy Priorities for Enhanced Rock Weathering↗</PDFLink>
+			</div>
+			<br>
+			<p>
+				Enhanced rock weathering (ERW) is a promising durable carbon removal and land management solution that involves spreading finely crushed alkaline rocks onto fields. By speeding up the natural weathering process of rocks, ERW can absorb CO2 from the atmosphere and durably store it for millennia. ERW can also help counteract widespread soil acidification by providing a cost-effective soil pH management solution to farmers. To unlock ERW’s full carbon and agronomic potential, governments should:</p>
+			<ul style="display: flex; flex-direction: column; gap: 1rem;">
+				<li><b>Invest in dedicated ERW research and development on carbon quantification, agronomic impacts, and environmental risks and co-benefits. </b> Publicly-funded R&D should target remaining uncertainties for ERW that the private sector is not well-positioned to address. In particular, long-term field trials and associated data collection and management will be critical to understand ERW’s carbon, agronomic, and environmental impact across a range of soil types, operational systems, and regional climates.</li>
+				<li><b>Provide financial assistance to address barriers to scale and support farmer adoption.:</b> Government financial assistance is needed to overcome barriers to scaling ERW—such as high measurement, reporting, and verification (MRV) costs—while supporting farmer adoption through new business models. This can take the form of funding for pilot projects, procurement policies, and direct compensation to farmers for ERW adoption.</li>
+				<li><b>Increase confidence of prospective carbon removal buyers and investors to enter the market through high-rigor standards and clear regulatory frameworks.</b> Uncertainty around the durability and credibility of carbon removal credits, a lack of regulatory clarity, and overall market immaturity have kept prospective CDR buyers and investors from entering the market. Governments can address these challenges through non-financial levers, including the development of high-rigor MRV standards, regulations with transparent timelines, and clear carbon accounting frameworks.</li>
+			</ul>
+			<p> </p>
+		</section>
+		<section id="scopeofWork">
+			<h1>Scope of Work</h1>
+			<div>Within super pollutant mitigation, we are building a bridge from catalytic, voluntary climate finance toward a policy-driven end state where accountability for super pollutant reductions is embedded within industry value chains and national greenhouse gas inventories. This strategy will require consistency and rigor in the accounting of short-lived climate pollutants in a framework that is built around CO2, a long-lived climate pollutant.
+
+                Credible accounting is the foundation for building confidence in the climate impact of any given intervention. We believe it is critical for the climate community to develop a shared mental model for accounting and claims-making across short-lived and long-lived mitigation activities, and a sense for the pros, cons, and implementation implications of the primary accounting option sets. 
+                
+                Why this is important:
+                
+                The range of GHG accounting approaches being considered is currently expanding rather than converging, creating risks of fragmentation, market uncertainty, and eroded confidence in mitigation efforts.
+                Market participants require clarity of options (and pros and cons for each) on how credits can be used and the “why” behind the investment case.
+                Getting to the desired end-state will require strong early project proof points to put in front of policymakers and industry, and providing clear and consistent rules of the road across project types will accelerate speed-to-action and help to mitigate accounting and market uncertainty-based paralysis. 
+                
+                Questions within this scope of work include: 
+                <ul>
+                <li>Should market-based actors consider changing accounting and claims rules to better value near term warming?</li>
+                <li>Do super pollutant reduction credits have a role in achieving net zero targets? If so, how?</li> 
+                <li>Should you allow compensation of CO2 emissions with super pollutant mitigation actions?</li>
+                </ul>
+                </div>
+			<PDFLink link="/Feedback on EU CRCF Review Paper on Carbon Removal through ERW.pdf" variant="accent" style="display: block; text-align: left;">Comment Letter:&nbsp; Feedback on EU CRCF Review Paper on Carbon Removal through ERW↗</PDFLink>
+			<PDFLink link="/Response to EU ETS Review Consultation_ Integrating Carbon Removal with Safeguards.pdf" variant="accent" style="display: block; text-align: left;">Comment Letter:&nbsp; Response to EU ETS Review Consultation Integrating Carbon Removal with Safeguards↗</PDFLink>
+			<PDFLink link="/Response to UNFCCC Article 6.4 Addressing Non-Permanence Reversals.pdf" variant="accent" style="display: block; text-align: left;">Comment Letter:&nbsp; Response to UNFCCC Article 6.4 Addressing Non-Permanence Reversals↗</PDFLink>
+			<PDFLink link="/Response to UNFCCC Article 6.4 Removal Guidance.pdf" variant="accent" style="display: block; text-align: left;">Comment Letter:&nbsp; Response to UNFCCC Article 6.4 Removal Guidance↗</PDFLink>
+			<PDFLink link="/Response to NRCS RFI on Climate-Smart Ag CPS.pdf" variant="accent" style="display: block; text-align: left;">Comment Letter:&nbsp; U.S. Department of Agriculture (USDA) on Implementation of the Conservation Practices to Support Climate Change Mitigation and Adaptation↗</PDFLink>
+			<PDFLink link="/GCSA RFI Response.pdf" variant="accent" style="display: block; text-align: left;">Comment Letter:&nbsp; USDA on the Greenhouse Gas Technical Assistance Provider and Third Party Verifier Program↗</PDFLink>
+			<br>
+			<div>Cascade also participates in formal government advisory groups and consortiums including:</div>
+			<ul>
+			<li>U.S. National Institute of Standards and Technology Carbon Dioxide Removal (CDR) Consortium</li>
+			<li>U.S. Pacific Northwest National Laboratory Mineralization CDR Measurement, Reporting, and Verification Collaborative</li>
+			<li>U.S. National Renewable Energy Laboratory Standardized Carbon Removal Offtake Agreement Advisory Board</li>
+			</ul>
+
+		</section>
+		<section id="nextHorizons">
+			<h1>Next Horizons</h1>
+			<p>
+				For Cascade, the overall goals of this accounting and claims effort is to enable higher overall participation in voluntary climate finance efforts directed towards high impact mitigation approaches, with strong guardrails in place against perverse incentives. 
+
+In addition to the workshop, Cascade is in the process of exploring which market and policy mechanisms — e.g., compliance carbon markets — can serve as testing grounds for how super pollutant mitigation can be effectively integrated and prioritized, and working to ensure that future demand pools are not unnecessarily conservative and restrictive in early rule setting.
+
+			</p>
+		</section>
+		<section id="resources">
+			<h1>Resources</h1>
+			<PDFLink link="/Cascade Climate_Enhanced Rock Weathering 101_vFinal.pdf" variant="accent">Enhanced Rock Weathering 101↗</PDFLink>
+			<PDFLink link="/Cascade Climate_Dissolved Carbon in the Oceans_vFinal.pdf" variant="accent">Dissolved Carbon in the Oceans↗</PDFLink>
+			<PDFLink link="/Cascade Climate_ERW Value Chain Graphic_vF.pdf" variant="accent">ERW Value Chain Graphic↗</PDFLink>
+			<PDFLink link="/CRA0225_Enhanced_Rock_Weathering.pdf" variant="accent">Enhanced Weathering Policy Working Group R&D Priorities↗</PDFLink>
+		</section>
+	</main>
+</div>
