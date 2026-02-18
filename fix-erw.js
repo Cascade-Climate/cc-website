4832be66b99fd89723c15db4fbfdc31fb762f026
jonjonmoore <JONATHAN@CASCADECLIMATE.ORG>
2026-02-18 12:31:25 -0500

    Revert "our work updates"
    This reverts commit 69485eeaf031925d4472df0a07b0d380d6982ff7.

--- fix-erw.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const p = 'src/routes/our-work/+page.svelte';
-let s = fs.readFileSync(p, 'utf8');
-s = s.replace(/<!--\s*ERW has potential[\s\S]*?\t\t\t<\/p>\n\t\t\t<h5>HIGHLIGHTS/, '\t\t\t<h5>HIGHLIGHTS');
-fs.writeFileSync(p, s);
-console.log('Done');
