72b4910496a3a44ef2aa798a9d1ffa8fa9dcfce3
jonjonmoore <JONATHAN@CASCADECLIMATE.ORG>
2026-04-27 15:35:20 -0400

    Refactor +page.svelte for improved layout and styling asabi

---
+++ src/routes/people/asabi-rawlins/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import linkedinIcon from '$lib/images/linkedin-icon.webp';
+</script>
+
+<svelte:head>
+	<title>Asabi Rawlins - Cascade Climate</title>
+	<meta
+		name="description"
+		content="Learn more about Asabi Rawlins's role at Cascade Climate."
+	/>
+</svelte:head>
+<div class="team-member">
+	<div class="member-info">
+		<h1>Asabi Rawlins</h1>
+		<div class="details">
+			<span>International Policy Manager</span>
+			<div class="socials">
+				<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/asabirawlins/">
+					<img src={linkedinIcon} alt="LinkedIn" />
+				</a>
+			</div>
+		</div>
+		<p>
+			With a decade of experience in international development, Asabi specializes in interdisciplinary policy and financing across multilaterals and development finance institutions. Prior to joining Cascade, Asabi held roles across UNDP, UNESCO, the International Renewable Energy Agency (IRENA), and other development organizations, and maintained an independent consulting practice on global development issues. Asabi holds an LLM from the University of Bristol (UK), where she was a Chevening Scholar, and an MSc from the University of the West Indies. She is also a Women in Power Fellow at the 92NY. 
+			
+			Originally from Trinidad and Tobago, Asabi is now based in Washington, DC.
+		</p>
+	</div>
+	<div class="headshot">
+		<img src="/images/people/asabi-rawlins.png" alt="Asabi Rawlins" />
+	</div>
+</div>
